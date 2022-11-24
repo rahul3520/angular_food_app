042c542a2fb98863bc724f4bba9bbc31ff5fd15f
rahulg3520 <rahul@nestgroup.net>
2022-11-25 00:46:23 +0530

    china food

---
+++ src/app/china-food/china-food.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-china-food',
+  templateUrl: './china-food.component.html',
+  styleUrls: ['./china-food.component.css']
+})
+export class ChinaFoodComponent {
+
+}
