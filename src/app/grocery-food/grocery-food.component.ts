9869ee4dedf095d4ca057edc81e8d2c525862214
rahulg3520 <rahul@nestgroup.net>
2022-11-25 01:05:04 +0530

    grocery food

---
+++ src/app/grocery-food/grocery-food.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grocery-food',
+  templateUrl: './grocery-food.component.html',
+  styleUrls: ['./grocery-food.component.css']
+})
+export class GroceryFoodComponent {
+
+}
